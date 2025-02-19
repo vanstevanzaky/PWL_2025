74c92d107dd8b0416db2e3944ce19fdff8bb5687
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-02-19 09:58:57 +0700

    route parameters d.

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,6 @@ Route::get('/user/{name}', function ($name) {
     return 'Nama Saya '.$name;
 });
 
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
