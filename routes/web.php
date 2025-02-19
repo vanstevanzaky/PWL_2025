6ddfdcce397d17d8b2650e29e2633077cf7e06f8
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-02-19 10:27:11 +0700

    optional parameters a.

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
 Route::get('/articles/{id}', function ($id) {
     return 'Halaman Artikel dengan ID '.$id;
 });
+
+Route::get('/user/{name?}', function ($name=null) { 
+    return 'Nama saya '.$name; 
+}); 
